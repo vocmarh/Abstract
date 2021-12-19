b98e4040f6af99574cc9eefbf37b2ef012fb8f1d
Sergei Khramtsov <vocmarh1989@gmail.com>
2021-12-19 21:16:08 +0500

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурка");
+            cat.Say();            
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }     
+
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название животного: {Name}, его звук: {0}");
+        }
+        
+    }
+    class Cat : Animal
+    {
+        string name;
+
+        public Cat(string name)
+        {
+            Name = name;            
+        }
+        
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+}
